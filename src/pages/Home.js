f5d01f2dd3acc42786aabe359ee0ef5ed0ccf346
FraWolf <frawolf@outlook.it>
2020-08-04 02:51:45 +0200

    Fixed pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { API_BASE } from './config.json';
+import { API_BASE } from '../config.json';
 
 class Home extends Component {
 
